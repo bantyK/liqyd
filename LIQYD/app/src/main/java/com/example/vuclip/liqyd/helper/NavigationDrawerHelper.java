9142193058e0262bf0977181e4b25cbaf6b6971d
bantyK <bantyp92@gmail.com>
2018-01-14 09:31:28 +0530

    1. Navigation drawer implemeted 2. Fragments added for terms conditions and order history

---
+++ LIQYD/app/src/main/java/com/example/vuclip/liqyd/helper/NavigationDrawerHelper.java
@@ -0,0 +1,9 @@
+package com.example.vuclip.liqyd.helper;
+
+/**
+ * Created by Banty on 14/01/18.
+ */
+
+public class NavigationDrawerHelper {
+
+}
---
+++ LIQYD/app/src/main/java/com/example/vuclip/liqyd/ui/gallary/TermsConditionsFragment.java
@@ -0,0 +1,25 @@
+package com.example.vuclip.liqyd.ui.gallary;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.vuclip.liqyd.R;
+
+/**
+ * Created by Banty on 14/01/18.
+ */
+
+public class TermsConditionsFragment extends Fragment {
+    public TermsConditionsFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.layout_terms_conditions, container, false);
+    }
+}
