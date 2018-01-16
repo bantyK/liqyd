914a8d0166cfb3aa90583cbedab4c24390ffbf86
bantyK <bantyp92@gmail.com>
2018-01-16 14:32:03 +0530

    order detail activity layout completed

--- LIQYD/app/src/main/java/com/example/vuclip/liqyd/ui/order_details/OrderDetailsActivity.java
+++ LIQYD/app/src/main/java/com/example/vuclip/liqyd/ui/order_details/OrderDetailsActivity.java
@@ -1,9 +1,14 @@
 package com.example.vuclip.liqyd.ui.order_details;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.vuclip.liqyd.R;
 import com.example.vuclip.liqyd.ui.BaseActivity;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class OrderDetailsActivity extends BaseActivity {
 
@@ -12,7 +17,23 @@ public class OrderDetailsActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_order_details_activity);
         configureToolbar(getString(R.string.order_details), true);
+
+        setUpSpinners();
     }
+
+
+    public void setUpSpinners() {
+        setUpDeliverySlotAdapter((Spinner) findViewById(R.id.delivery_slot_spinner), Arrays.asList(getResources().getStringArray(R.array.delivery_slots)));
+        setUpDeliverySlotAdapter((Spinner) findViewById(R.id.delivery_type_spinner), Arrays.asList(getResources().getStringArray(R.array.delivery_types)));
+        setUpDeliverySlotAdapter((Spinner) findViewById(R.id.delivery_time_spinner), Arrays.asList(getResources().getStringArray(R.array.delivery_time)));
+    }
+
+    private void setUpDeliverySlotAdapter(Spinner spinner, List<String> spinnerData) {
+        ArrayAdapter<String> deliverySlotAdapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, spinnerData);
+        spinner.setAdapter(deliverySlotAdapter);
+    }
+
 
     @Override
     protected int getLayoutResource() {
