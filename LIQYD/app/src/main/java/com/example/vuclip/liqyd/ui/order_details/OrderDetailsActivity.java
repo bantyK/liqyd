ef15053968b2ac8d356aedf008d7fa3a42c6d1d8
bantyK <bantyp92@gmail.com>
2018-01-16 15:54:44 +0530

    order detail quantity buttons click handling done

--- LIQYD/app/src/main/java/com/example/vuclip/liqyd/ui/order_details/OrderDetailsActivity.java
+++ LIQYD/app/src/main/java/com/example/vuclip/liqyd/ui/order_details/OrderDetailsActivity.java
@@ -1,8 +1,11 @@
 package com.example.vuclip.liqyd.ui.order_details;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.example.vuclip.liqyd.R;
 import com.example.vuclip.liqyd.ui.BaseActivity;
@@ -11,14 +14,44 @@ import java.util.Arrays;
 import java.util.List;
 
 public class OrderDetailsActivity extends BaseActivity {
+    private Button quantityPlusButton, quantityMinusButton;
+    private TextView orderQuantity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_order_details_activity);
         configureToolbar(getString(R.string.order_details), true);
-
+        initUIElements();
         setUpSpinners();
+
+    }
+
+    private void initUIElements() {
+        quantityPlusButton = findViewById(R.id.quantity_plus_button);
+        quantityMinusButton = findViewById(R.id.quantity_minus_button);
+        orderQuantity = findViewById(R.id.order_quantity);
+
+        View.OnClickListener quantityButtonClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int currentQuantityValue = Integer.parseInt(orderQuantity.getText().toString());
+                switch (v.getId()) {
+                    case R.id.quantity_plus_button:
+                        currentQuantityValue = currentQuantityValue + 1;
+                        break;
+                    case R.id.quantity_minus_button:
+                        if (currentQuantityValue != 2)
+                            currentQuantityValue = currentQuantityValue - 1;
+                        break;
+                }
+
+                orderQuantity.setText(String.valueOf(currentQuantityValue));
+            }
+        };
+        quantityPlusButton.setOnClickListener(quantityButtonClickListener);
+        quantityMinusButton.setOnClickListener(quantityButtonClickListener);
+
     }
 
 
