a4423b33a80b6c7a33839b853082e7dbcb0c4696
bantyK <bantyp92@gmail.com>
2018-01-17 08:01:36 +0530

    payment screen UI done

--- LIQYD/app/src/main/java/com/example/vuclip/liqyd/ui/payment/PaymentActivity.java
+++ LIQYD/app/src/main/java/com/example/vuclip/liqyd/ui/payment/PaymentActivity.java
@@ -1,17 +1,40 @@
 package com.example.vuclip.liqyd.ui.payment;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 
 import com.example.vuclip.liqyd.R;
 import com.example.vuclip.liqyd.ui.BaseActivity;
 
 public class PaymentActivity extends BaseActivity {
 
+    CheckBox promoCodeCheckBox;
+    EditText promoCodeEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_payment_activity);
         configureToolbar(getString(R.string.layout_payment_activity), true);
+        initUIElements();
+    }
+
+    private void initUIElements() {
+        promoCodeCheckBox = findViewById(R.id.cb_promo_code);
+        promoCodeEditText = findViewById(R.id.et_promo_code);
+
+        promoCodeCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked && promoCodeEditText.getVisibility() == View.GONE){
+                    promoCodeEditText.setVisibility(View.VISIBLE);
+                } else if(!isChecked && promoCodeEditText.getVisibility() == View.VISIBLE) {
+                    promoCodeEditText.setVisibility(View.GONE);
+                }
+            }
+        });
     }
 
     @Override
