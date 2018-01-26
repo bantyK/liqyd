273b46f98418de9c9048684c3b0188bd0502b863
bantyK <bantyp92@gmail.com>
2018-01-26 15:41:29 +0530

    onActivityResult error
    (cherry picked from commit 61b81ac)

--- LIQYD/app/src/main/java/com/example/vuclip/liqyd/user/UserConstants.java
+++ LIQYD/app/src/main/java/com/example/vuclip/liqyd/user/UserConstants.java
@@ -11,5 +11,5 @@ public class UserConstants {
     public static final String USER_PASSWORD = "user.password";
     public static final String USER_ADDRESS_1 = "user.address.1";
     public static final String USER_ADDRESS_2 = "user.address.2";
-    public static final String USER_LANDMARK = "user.landmark";
+    public static final String USER_ADDRESS_LANDMARK = "user.landmark";
 }
